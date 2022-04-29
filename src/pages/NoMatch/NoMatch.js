3d613c15b9c76f56ea368499e792a9860692b4c1
g322h <g2203h@gmail.com>
2022-04-29 21:34:44 +0300

    styled no match page (404)

--- src/pages/NoMatch/NoMatch.js
+++ src/pages/NoMatch/NoMatch.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import s from "./NoMatch.module.css";
 
 export const NoMatch = () => {
-    return <h1>СТРАНИЦА НЕ НАЙДЕНА</h1>;
+    const history = useHistory();
+    return (
+        <div className={s.noMatch}>
+            <h1 className={s.title}>404. Страница не найдена</h1>
+            <h4 className={s.subtitle}>
+                Возможно, она была перемещена, или вы просто неверно указали
+                адрес страницы.
+            </h4>
+            <button className={s.button} onClick={() => history.goBack()}>
+                Назад
+            </button>
+        </div>
+    );
 };
